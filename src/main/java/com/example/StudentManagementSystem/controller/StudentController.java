bba24993f85276e305f33b9697d73f2d7a0be199
Rishav Karanjit <karanjitrishav4@gmail.com>
2022-12-29 22:56:15 -0800

    changed path to ""

--- src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
+++ src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping(path = "/abc")
+@RequestMapping(path = "")
 public class StudentController {
     @ResponseBody
     @GetMapping
