4d4dbdaf0456c04644a1b81182f4a50fb74e48b5
Rishav Karanjit <karanjitrishav4@gmail.com>
2022-12-31 11:12:59 -0800

    Added function to get all students

--- src/main/java/com/example/StudentManagementSystem/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/StudentManagementSystem/service/impl/StudentServiceImpl.java
@@ -1,2 +1,22 @@
-package com.example.StudentManagementSystem.service.impl;public class StudentServiceImpl {
+package com.example.StudentManagementSystem.service.impl;
+
+import com.example.StudentManagementSystem.entity.Student;
+import com.example.StudentManagementSystem.repository.StudentRepository;
+import com.example.StudentManagementSystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class StudentServiceImpl implements StudentService {
+    private StudentRepository studentRepository;
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
