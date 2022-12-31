c9ff5c8f498499210445e518e6ebb9aabce9ab1f
Rishav Karanjit <karanjitrishav4@gmail.com>
2022-12-30 22:26:37 -0800

    Displayed hello world from controller

--- src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
+++ src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.example.StudentManagementSystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequestMapping(path = "")
 public class StudentController {
-    @GetMapping
-    public String home(){
+    @GetMapping("/")
+    public String home(Model modelMap){
+        modelMap.addAttribute("data","Hello World");
         return "index";
     }
 }
