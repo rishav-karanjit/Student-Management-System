b259f6197e24a4b799939378451aa756fb009a09
Rishav Karanjit <karanjitrishav4@gmail.com>
2022-12-31 11:32:15 -0800

    Added commandLineRunner to add entries to db

--- src/main/java/com/example/StudentManagementSystem/StudentConfig.java
+++ src/main/java/com/example/StudentManagementSystem/StudentConfig.java
@@ -1,2 +1,36 @@
-package com.example.StudentManagementSystem;public class StudentConfig {
+package com.example.StudentManagementSystem;
+
+import com.example.StudentManagementSystem.entity.Student;
+import com.example.StudentManagementSystem.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student abc = new Student(
+                    "abc",
+                    "abc@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    25
+            );
+
+            Student def = new Student(
+                    "def",
+                    "def@gmail.com",
+                    LocalDate.of(2001, Month.JANUARY, 5),
+                    25
+            );
+
+            repository.saveAll(
+                    List.of(abc,def)
+            );
+        };
+    }
 }
