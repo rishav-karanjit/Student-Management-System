6fa09e1a85597e0f974e2e248f6f0b186f3334e1
Rishav Karanjit <karanjitrishav4@gmail.com>
2022-12-31 11:14:14 -0800

    Added ListStudents in controller

--- src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
+++ src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
@@ -1,19 +1,29 @@
 package com.example.StudentManagementSystem.controller;
 
+import com.example.StudentManagementSystem.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping(path = "")
 public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping("/")
-    public String home(Model modelMap){
-        modelMap.addAttribute("data","Hello World");
+    public String home(Model model){
+
+        model.addAttribute("data","Hello World");
         return "index";
+    }
+    @GetMapping("/student")
+    public String ListStudents(Model model){
+        model.addAttribute("student",studentService.getAllStudents());
+        return "AllStudents";
     }
 }
