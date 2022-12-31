0b9359359493d97071aba818b8458818ddc17314
Rishav Karanjit <karanjitrishav4@gmail.com>
2022-12-31 10:48:13 -0800

    Added service interface

---
+++ src/main/java/com/example/StudentManagementSystem/service/StudentService.java
@@ -0,0 +1,9 @@
+package com.example.StudentManagementSystem.service;
+
+import com.example.StudentManagementSystem.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+}
